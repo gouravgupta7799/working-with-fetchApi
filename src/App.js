11352017478dbdb8355383513d8175df5453aa7d
Gourav Gupta <gouravgupta7799@gmail.com>
2024-01-17 17:06:07 +0530

    delete post get method

--- src/App.js
+++ src/App.js
@@ -8,25 +8,29 @@ function App() {
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [FormShow, setFormShow] = useState(false);
+  const url = 'https://fetchapiproject-1ef23-default-rtdb.firebaseio.com/movies.json';
 
+  // fetching get request
   const movieHandler = useCallback(async () => {
     try {
       setLoading(true)
       setError(null)
-      let newData = await fetch('https://swapi.dev/api/films')
+      let newData = await fetch(url)
 
       if (!newData.ok) {
         throw new Error('Something went wrong ....Retrying')
       }
       let data = await newData.json()
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          release_Date: movieData.release_date
-        };
-      })
+      const transformedMovies = []
+      for (let i in data) {
+        console.log(data[i])
+        transformedMovies.push({
+          id: i,
+          title: data[i].title,
+          openingText: data[i].openingText,
+          release_Date: data[i].releaseDate
+        });
+      }
       setDummyMovies(transformedMovies)
     }
     catch (error) {
@@ -34,6 +38,55 @@ function App() {
     }
     setLoading(false)
   }, [])
+
+
+  // fetching post request
+  const addMovieHandler = async (movie) => {
+    try {
+      setFormShow(!FormShow)
+      const res = await fetch(url,
+        {
+          method: 'POST',
+          body: JSON.stringify(movie)
+        },
+        {
+          Headers: {
+            'content-type': 'application/Json'
+          }
+        })
+      if (res.status === 200) {
+        setDummyMovies([movie, ...dummyMovies])
+      }
+    }
+    catch (err) {
+      console.log(err.message)
+    }
+  }
+
+  
+  // fetching delete request
+  const deleteMovieHandler = async (id) => {
+    try {
+      const res = await fetch(url,
+        {
+          method: 'DELETE',
+          body: JSON.stringify(id)
+        },
+        {
+          Headers: {
+            'content-type': 'application/Json'
+          }
+        })
+      if (res.status === 200) {
+        setDummyMovies(dummyMovies.filter(movie => movie.id !== id))
+}
+    }
+    catch (err) {
+      console.log(err.message)
+    }
+  }
+
+
   let content = <p>Found no moives.</p>
 
   const moviesListMemo = useMemo(() => {
@@ -41,7 +94,7 @@ function App() {
   }, [dummyMovies])
 
   if (!isLoading && dummyMovies.length > 0 && moviesListMemo) {
-    content = <MoviesList movies={dummyMovies} />
+    content = <MoviesList movies={dummyMovies} deleteMovieHandler={deleteMovieHandler} />
   }
   if (error) {
     content = <div>
@@ -56,10 +109,7 @@ function App() {
   function showForm() {
     setFormShow(!FormShow)
   }
-  function addMovieHandler(movie) {
-    console.log(movie)
-    setFormShow(!FormShow)
-  }
+
 
   useEffect(() => {
     movieHandler()
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,12 +3,25 @@ import React from 'react';
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+
+  function deleteHandler(id) {
+    props.deleteMovie(id)
+  }
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
-    </li>
+      <button style={{
+        'font': 'inherit',
+        'cursor': 'pointer',
+        'background': 'red',
+        'border': '1px solid black',
+        'color': 'white',
+        'padding': '0.50rem 1rem',
+        'borderRadius': '20px',
+      }} onClick={() => deleteHandler(props.id)}>delete</button>
+    </li >
   );
 };
 
